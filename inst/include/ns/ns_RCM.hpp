ad993d6f7d476aec88240c9b61d37c73a9c948c9
Quang Huynh <quang@bluematterscience.com>
2022-06-07 12:54:37 -0700

    Conver TMB integers to Type

--- inst/include/ns/ns_RCM.hpp
+++ inst/include/ns/ns_RCM.hpp
@@ -265,29 +265,29 @@ Type comp_mvlogistic(array<Type> obs, array<Type> pred, matrix<Type> N, int n_y,
     
     for(int bb=0;bb<n_bin;bb++) {
       Type p_pred = pred(y,bb,ff)/N(y,ff);
-      tmp(y,bb) = CppAD::CondExpGt(obs(y,bb,ff), p_min, log(obs(y,bb,ff)) - log(p_pred), 0); // Residual
+      tmp(y,bb) = CppAD::CondExpGt(obs(y,bb,ff), p_min, log(obs(y,bb,ff)) - log(p_pred), Type(0)); // Residual
       
-      accum_obs += CppAD::CondExpLe(obs(y,bb,ff), p_min, obs(y,bb,ff), 0);
-      accum_pred += CppAD::CondExpLe(obs(y,bb,ff), p_min, p_pred, 0);
-      A(y) += CppAD::CondExpGt(obs(y,bb,ff), p_min, 1, 0); // Zero if no data in year y
+      accum_obs += CppAD::CondExpLe(obs(y,bb,ff), p_min, obs(y,bb,ff), Type(0));
+      accum_pred += CppAD::CondExpLe(obs(y,bb,ff), p_min, p_pred, Type(0));
+      A(y) += CppAD::CondExpGt(obs(y,bb,ff), p_min, Type(1), Type(0)); // Zero if no data in year y
       
       sum_term(y) += tmp(y,bb);
     }
     
-    A(y) += CppAD::CondExpGt(accum_obs, Type(0), 1, 0); // One if no data in year y
+    A(y) += CppAD::CondExpGt(accum_obs, Type(0), Type(1), Type(0)); // One if no data in year y
     sum_term(y) += CppAD::CondExpGt(accum_obs, Type(0), log(accum_obs) - log(accum_pred), Type(0));
     sum_term(y) /= A(y);
     
     // Calculate eta iff A(y) > 1
     for(int bb=0;bb<n_bin;bb++) {
-      tau2 += CppAD::CondExpGt(obs(y,bb,ff), p_min, (tmp(y,bb) - sum_term(y)) * (tmp(y,bb) - sum_term(y)), 0);
+      tau2 += CppAD::CondExpGt(obs(y,bb,ff), p_min, (tmp(y,bb) - sum_term(y)) * (tmp(y,bb) - sum_term(y)), Type(0));
     }
     
     tau2 += CppAD::CondExpGt(A(y), Type(1), 
                              (log(accum_obs) - log(accum_pred) - sum_term(y)) * (log(accum_obs) - log(accum_pred) - sum_term(y)), 
-                              0);
+                              Type(0));
     
-    sum_count += CppAD::CondExpGt(A(y), Type(1), A(y) - 1, 0);
+    sum_count += CppAD::CondExpGt(A(y), Type(1), A(y) - 1, Type(0));
   }
   tau2 /= sum_count;
   
